05437f07f801306541cf5f0bad86445660668ac7
manu10210 <claustre.emmanuel@gmail.com>
2026-04-15 20:02:47 +0200

    chore: remove temp scripts

--- fix_timeout.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('components/GmailImporter.tsx', 'utf8');
-
-// Insert end of importing logic at the very end of importSelected logic (around line 905)
-code = code.replace(/(setCurrentWizard\(queue\[0\]\);\s*\n\s*\})/, "$1\n\n      setTimeout(() => setStatus('idle'), 2500);\n      setStatus('done');");
-
-fs.writeFileSync('components/GmailImporter.tsx', code);
